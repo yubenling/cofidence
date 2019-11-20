ff492e0361b7e8f93013e5769cf21fb5b8d48a1b
yubenling <yubenling1004@163.com>
2019-11-20 21:36:54 +0800

    添加springcloud的config

--- confidence-config/src/main/java/com/cloud/confidence/confidenceconfig/ConfidenceConfigApplication.java
+++ confidence-config/src/main/java/com/cloud/confidence/confidenceconfig/ConfidenceConfigApplication.java
@@ -2,8 +2,14 @@ package com.cloud.confidence.confidenceconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableConfigServer
 public class ConfidenceConfigApplication {
 
     public static void main(String[] args) {
