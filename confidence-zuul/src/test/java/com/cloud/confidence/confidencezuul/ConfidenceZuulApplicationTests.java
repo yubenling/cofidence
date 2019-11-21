2c8517f651bebdf25a4f54fbcbb32c348d28cffb
yubenling <yubenling1004@163.com>
2019-11-21 21:24:08 +0800

    springcloudzuul

---
+++ confidence-zuul/src/test/java/com/cloud/confidence/confidencezuul/ConfidenceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.confidence.confidencezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfidenceZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
