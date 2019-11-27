e4123f7bc260a0df70a3f78a64ad87898d8112f6
yubenling <yubenling1004@163.com>
2019-11-27 19:36:41 +0800

    util工具类

---
+++ confidence-util/src/test/java/com/cloud/confidence/confidenceutil/ConfidenceUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.confidence.confidenceutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfidenceUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
